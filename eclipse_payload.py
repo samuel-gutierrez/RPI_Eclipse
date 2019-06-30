4b427f98cae7dfcb089110ee2d719178f6c75c45
samuel-gutierrez <samuel.gutierrez@usach.cl>
2019-06-29 22:52:37 -0400

    Upgrading sun_functions. The code is now more robust.

--- eclipse_payload.py
+++ eclipse_payload.py
@@ -3,6 +3,7 @@ import cv2
 import time
 import smbus
 import sun_functions
+import datetime as dt
 import multiplexer_functions
 from eclipse_class import PiCameraStream
 
@@ -20,8 +21,9 @@ class EclipsePayload:
         self.ePin = ePin
         self.bus = smbus.SMBus(1)
         multiplexer_functions.start_multiplexer(self.bus, self.f1Pin, self.f2Pin, self.ePin)
-        self.vs = PiCameraStream(bright=50)
-        self.last_pic = ''
+        self.vs = PiCameraStream()
+        self.sun_last_pic = ''
+        self.test_last_pic = ''
 
 
     def reset_defaults_parameters(self, f1Pin=33, f2Pin=31, ePin=15):
@@ -34,15 +36,18 @@ class EclipsePayload:
         self.bus = smbus.SMBus(1)
         multiplexer_functions.start_multiplexer(self.bus, self.f1Pin, self.f2Pin, self.ePin)
         self.vs = PiCameraStream()
-        self.last_pic = ''
-      
+        self.sun_last_pic = ''
+        self.test_last_pic = ''
 
-    def start(self, r_sun=150, sun_img_dir='/home/pi/Sun_pictures'):
+
+    def start(self):
         """
         Initialize continuous capture from camera and sun analyzing. 
         """
         try:
-          self.sun_capture()
+            while True:
+                self.sun_capture()
+                self.take_test_pictures()
         except Exception as e:
             print 'There is an exception: ', str(e)
             self.restart()
@@ -64,60 +69,65 @@ class EclipsePayload:
         self.reset_defaults_parameters()
         time.sleep(1.0)
         self.start()
-        
-    
+
+
     def sun_capture(self, r_sun=150, sun_img_dir='/home/pi/Sun_pictures'):
         self.vs.start()
-        print '- OK - '
         stream = self.vs.stream
         cn = 0
-        r_sun = r_sun
         camera_number = 1
         multiplexer_functions.select_camera(self.bus, self.f1Pin, self.f2Pin, self.ePin, camera_number)
         time.sleep(1.0)
         for (ii, f) in enumerate(stream):
             frame = f.array
             print 'Frame number: ', ii, ' / Camera number: ', camera_number
-            frame_pic_dir = '/home/pi/Frame_pictures/frame_%d_cam_%d.jpg' %(ii, camera_number)
-            cv2.imwrite(frame_pic_dir, frame)
+            # frame_pic_dir = '/home/pi/Frame_pictures/frame_%d_cam_%d.jpg' %(ii, camera_number)
+            # cv2.imwrite(frame_pic_dir, frame)
             try:
                 crop_sun_img = sun_functions.sun_detect(frame, r_sun)
                 aux_counter = 1 + ii - cn
-                aux_dir = sun_img_dir + '/img_%d_frame_%d_cam_%d.jpg' % (aux_counter, ii, camera_number)
+                dtime = dt.datetime.now()
+                str_dtime = str(dtime.hour) + '_' + str(dtime.minute) + '_' + str(dtime.second)
+                aux_dir = sun_img_dir + '/img_%d_frame_%d_cam_%d_' + str_dtime + '.jpg' % (aux_counter, ii, camera_number)
                 cv2.imwrite(aux_dir, crop_sun_img)
-                self.last_pic = aux_dir
+                self.sun_last_pic = aux_dir
             except Exception as e:
                 cn += 1
                 print 'There is an exception: ', str(e)
                 camera_number = (cn%4) + 1
                 multiplexer_functions.force_camera_change(self.bus, self.f1Pin, self.f2Pin, self.ePin, camera_number)
             self.vs.rawCapture.truncate(0)
+            if ii == 500:
+                break
 
 
-    def take_test_pictures(self, test_img_dir='/home/pi/test_pictures'):
+    def take_test_pictures(self, test_img_dir='/home/pi/Test_pictures'):
         """
         Stop the analysis of pictures of the sun, and take normal pictures with the 4 RPI cameras.
         """
         self.stop
         time.sleep(1.0)
+        dtime = dt.datetime.now()
+        str_dtime = str(dtime.hour) + '_' + str(dtime.minute) + '_' + str(dtime.second)
         img_task = 'raspistill -n -t 1 -o ' + test_img_dir
         multiplexer_functions.gpio_setup(self.f1Pin, self.f2Pin, self.ePin)
         multiplexer_functions.select_camera(self.bus, self.f1Pin, self.f2Pin, self.ePin, 1)
-        img_task1 = img_task + '/img_cam_1.jpg'
+        img_task1 = img_task + '/img_cam_1_time_' + str_dtime + '.jpg'
         os.system(img_task1)
         time.sleep(1.0)
         multiplexer_functions.select_camera(self.bus, self.f1Pin, self.f2Pin, self.ePin, 2)
-        img_task2 = img_task + '/img_cam_2.jpg'
+        img_task2 = img_task + '/img_cam_2_time_' + str_dtime + '.jpg'
         os.system(img_task2)
         time.sleep(1.0)
         multiplexer_functions.select_camera(self.bus, self.f1Pin, self.f2Pin, self.ePin, 3)
-        img_task3 = img_task + '/img_cam_3.jpg'
+        img_task3 = img_task + '/img_cam_3_' + str_dtime + '.jpg'
         os.system(img_task3)
         time.sleep(1.0)
         multiplexer_functions.select_camera(self.bus, self.f1Pin, self.f2Pin, self.ePin, 4)
-        img_task4 = img_task + '/img_cam_4.jpg'
+        img_task4 = img_task + '/img_cam_4_' + str_dtime + '.jpg'
         os.system(img_task4)
         time.sleep(1.0)
+        self.test_last_pic = test_img_dir + '/img_cam_X_' + str_dtime + '.jpg' 
         multiplexer_functions.gpio_set_camera(self.f1Pin, self.f2Pin, self.ePin, 1)
         time.sleep(1.0)
         self.reset_defaults_parameters()
@@ -127,4 +137,3 @@ if __name__ == "__main__":
     task = EclipsePayload()
     task.start()
     task.stop()
-    
--- sun_functions.py
+++ sun_functions.py
@@ -2,25 +2,7 @@
 A set of functions used to detect the sun in an image
 """
 import cv2
-
-
-def test_sat(thresh, sizex, sizey, len_sat=400):
-    """
-    Test if the image is over saturated
-    -----------------------------
-    Inputs : Thresholded image, image size, saturation lenght 
-    Outputs: 1 if image is saturated
-             0 if not
-    """
-    aux1 = min(sizex, sizey)
-    counter = 0
-    for ii in range(aux1):
-        counter += thresh[ii][ii]/255
-    if counter > len_sat:
-        aux2 = 1
-    else:
-        aux2 = 0
-    return aux2
+import numpy as np
 
 
 def contour_function(thresh):
@@ -50,9 +32,68 @@ def test_edge_sun(size_x, size_y, center_x, center_y, cut_radius):
     return value
 
 
+def find_local_max(array, local_max_tol=50):
+    """
+    Find all the local maximum in the histogram array
+    -----------------------------
+    """
+    dev1 = np.gradient(array)
+    dev2 = np.gradient(dev1)
+    list_local_max = []
+    list_index_local_max = []
+    for ii in range(len(array)):
+        if dev1[ii] > local_max_tol and dev2[ii] < 0:
+            list_local_max.append(array[ii+1])
+            list_index_local_max.append(ii+1)
+    return list_local_max, list_index_local_max
+
+
+def intensity(img_bw):
+    """
+    Find the intensity of the last (sun) local max in the histogram of the picture
+    -----------------------------
+    Input: bw image (np.ndararry)
+    Return: local_max (int [0, 256])
+    """
+    hist = cv2.calcHist([img_bw], [0], None, [256], [0,256])
+    hist_array = np.concatenate(hist)
+    hist_array = np.append(hist_array, 0)
+    local_max_values, local_max_index = find_local_max(hist_array)
+    index = local_max_index[-1]
+    good_index = index - 5
+    if good_index < 0:
+        good_index = 0
+    return good_index
+
+
+def is_circle(contour, circle_tolerance_factor=0.2, count_factor_tolerance=0.2):
+    """
+    Verify if a contour has a circle shape
+    -----------------------------
+    Input: contour (np.ndarray), circle_tolerance_factor (float), count_tolerance_factor (float)
+    Return: stat ---> 0 if is circle,
+                      1 if doesnt
+    """
+    pos, radius = cv2.minEnclosingCircle(contour)
+    R = int(radius)
+    x_center = int(pos[0])
+    y_center = int(pos[1])
+    count = 0
+    stat = 0
+    perimeter = len(contour)
+    count_tolerance = count_factor_tolerance * perimeter
+    for point in contour:
+        diff = ((point[0][0] - x_center)**2 +  (point[0][1] - y_center)**2)**(0.5)
+        if abs(diff - R) > circle_tolerance_factor*R:
+            count += 1
+        if count > count_tolerance:
+            stat = 1
+    return stat, x_center, y_center, R
+
+
 def is_sun(cnts, r_sun):
     """
-    Test if any contour have a similar size to the sun
+    Checks whether a contour have size and shape similar to the sun
     -----------------------------
     Input : List of contours, estimated radius of the sun in the image
     Output: Status (= 0 if similar; = 1 if not), sun shape
@@ -63,34 +104,33 @@ def is_sun(cnts, r_sun):
     R = 0
     sorted_contours = sorted(cnts, key=cv2.contourArea, reverse=True)
     for contour in sorted_contours:
-        pos, radius = cv2.minEnclosingCircle(contour)
-        R = int(radius)
-        if 5 <= R <= r_sun:
-            x_center = int(pos[0])
-            y_center = int(pos[1])
+        iscircle, x_center, y_center, R = is_circle(contour)
+        if iscircle == 0 and (5 <= R <= r_sun):
             stat = 0
             break
     return stat, x_center, y_center, R
 
 
-def sun_detect(img, r_sun):
+def sun_detect(img, r_sun, cut_sun_factor=1.4):
     """
-    Developed algorithm to detect and cut the sun in the image
+    Developed algorithm to detect and cut the sun in the RPI image
     -----------------------------
     Input : Image, estimated radius of the sun in the image
     Output: B&W cropped image of the sun (np.ndarray)
     """
     img_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     size_y, size_x, chan = img.shape
-    thresh = cv2.inRange(img_bw, (200), (255))
+    intensity_value = intensity(img_bw)
+    if intensity_value == 0:
+        raise ValueError(' ==> Error: Can not find a good intensity threshold value in the image.')
+    thresh = cv2.inRange(img_bw, intensity_value, 255)
     contours = contour_function(thresh)
     if len(contours) == 0:
-        raise ValueError(' ==> Error: There is no sun in picture.')
+        raise ValueError(' ==> Error: Can not find any contours in the thresholded image.')
     stat, center_x, center_y, radius = is_sun(contours, r_sun)
     if stat == 1:
-        raise ValueError(' ==> Error: Can not find the sun!')
-    cut_factor = 1.3
-    cut_radius = int(cut_factor * radius)
+        raise ValueError(' ==> Error: The algorithm can not find the sun shape in the image.')
+    cut_radius = int(cut_sun_factor * radius)
     test_edge = test_edge_sun(size_x, size_y, center_x, center_y, cut_radius) 
     if test_edge == 1:
         raise ValueError(' ==> Error: The sun is in the edge of the picture.')
